369bdf75e04e96136dd85918920018bb7d608fbe
Urvashi <schrodinger@Urvashi.local>
2024-12-16 23:14:57 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["Problem Solver","Machine Learning Enthusiast", "Sketch Artist", "Youtuber", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
